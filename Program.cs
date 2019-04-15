cc2d7696ad870d179bac82d7a2781c8ffc71364d
Lamond Lu <lunan@shintechchina.com>
2019-04-15 22:03:26 +0800

    update the code

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace DroneCIIntegration
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://+:5010")
                 .UseStartup<Startup>();
     }
 }
